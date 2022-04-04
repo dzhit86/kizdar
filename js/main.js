609e802092002eedd9adaefeaf00102d8e54e658
dzhit86 <dzhit86@gmail.com>
2022-04-04 21:00:54 +0600

    замена слайдера

--- js/main.js
+++ js/main.js
@@ -1,103 +1,228 @@
-"use strict";
+$(document).ready(function() {
+  const cityCurrent = $(".select-block__current");
+  const citySearch = $("#citySearch");
+  const cityList = $("#cityList");
+  const menuWrap = $(".select-block__wrap");
 
-$(document).ready(function () {
-  var cityCurrent = $(".select-block__current");
-  var citySearch = $("#citySearch");
-  var cityList = $("#cityList");
-  var menuWrap = $(".select-block__wrap");
-  cityCurrent.on("click", function (e) {
+  cityCurrent.on("click", function(e){
     e.preventDefault();
     $(this).parent().toggleClass("_active");
-
     if (!$(this).parent().hasClass("_active")) {
       citySearch.val("");
       cityList.find("a").show();
     }
   });
 
-  (function () {
-    citySearch.on("keyup", function () {
-      var value = $(this).val().toLowerCase();
-      cityList.find("a").filter(function () {
+  (function(){
+    citySearch.on("keyup", function() {
+      let value = $(this).val().toLowerCase();
+      cityList.find("a").filter(function() {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
       });
     });
   })();
 
-  (function () {
-    $(".profile-phone__trigger").on("click", function (e) {
+  (function(){
+    $(".profile-phone__trigger").on("click", function(e) {
       e.preventDefault();
-      console.log($(this).text());
-      $(this).siblings(".profile-phone__short").css("display", "none");
-      $(this).css("display", "none");
-      $(this).siblings(".profile-phone__full").css("display", "block");
-    });
+      console.log($(this).text())
+      $(this).siblings(".profile-phone__short").css("display","none");
+      $(this).css("display","none");
+      $(this).siblings(".profile-phone__full").css("display","block");
+
+    })
   })();
 
-  (function () {
-    var reReplyForm = $("\n      <div class=\"comment-re\">\n        <form class=\"comment-re__form\" name=\"\" action=\"\" method=\"POST\">\n          <textarea class=\"comment-re__textarea\" name=\"\" required></textarea>\n          <button class=\"comment-re__send btn btn_primary\">\u041E\u0442\u043F\u0440\u0430\u0432\u0438\u0442\u044C</button>\n        </form>\n      </div>\n    ");
-    $(".comment__item").on("click", ".comment__reply", function (e) {
+  (function(){
+    const reReplyForm = $(`
+      <div class="comment-re">
+        <form class="comment-re__form" name="" action="" method="POST">
+          <textarea class="comment-re__textarea" name="" required></textarea>
+          <button class="comment-re__send btn btn_primary">Отправить</button>
+        </form>
+      </div>
+    `);
+
+    $(".comment__item").on("click", ".comment__reply", function(e){
       e.preventDefault();
       $(document).find(".comment-re").remove();
-      $(".comment__reply_cancel").replaceWith("<a class=\"comment__reply\" href=\"#\">\u041E\u0442\u0432\u0435\u0442\u0438\u0442\u044C</a>");
+      $(".comment__reply_cancel").replaceWith(`<a class="comment__reply" href="#">Ответить</a>`);
+
       $(this).after(reReplyForm);
-      $(this).replaceWith("<a class=\"comment__reply_cancel\" href=\"#\">\u041E\u0442\u043C\u0435\u043D\u0430</a>");
+      $(this).replaceWith(`<a class="comment__reply_cancel" href="#">Отмена</a>`);
     });
-    $(".comment__item").on("click", ".comment__reply_cancel", function (e) {
+
+    $(".comment__item").on("click",".comment__reply_cancel", function(e){
       e.preventDefault();
       $(document).find(".comment-re").remove();
-      $(this).replaceWith("<a class=\"comment__reply\" href=\"#\">\u041E\u0442\u0432\u0435\u0442\u0438\u0442\u044C</a>");
+      $(this).replaceWith(`<a class="comment__reply" href="#">Ответить</a>`);
     });
   })();
 
-  (function () {
-    var topMenu = $(".header__nav");
-    var btnBurger = $(".header__burger");
+
+  (function(){
+
+    const topMenu = $(".header__nav");
+    const btnBurger = $(".header__burger");
+  
     btnBurger.on("click", mobileMenu);
-
+  
     function mobileMenu(event) {
-      event.preventDefault();
-      btnBurger.add(topMenu).toggleClass("_active");
+        event.preventDefault();
+        btnBurger.add(topMenu).toggleClass("_active");
     }
-
     function closeMenu() {
-      btnBurger.add(topMenu).removeClass("_active");
+        btnBurger.add(topMenu).removeClass("_active");
     }
+  
+    $(window).resize(function () { 
+        if ($(this).outerWidth() >= 991) {
+            closeMenu();
+        }
+    });
 
-    $(window).resize(function () {
-      if ($(this).outerWidth() >= 991) {
-        closeMenu();
-      }
-    });
     $(document).mouseup(function (e) {
-      if (!topMenu.is(e.target) && topMenu.has(e.target).length === 0 && !btnBurger.is(e.target) && btnBurger.has(e.target).length === 0) {
-        closeMenu();
+      if (!topMenu.is(e.target)
+          && topMenu.has(e.target).length === 0
+          && !btnBurger.is(e.target)
+          && btnBurger.has(e.target).length === 0) {
+              closeMenu();
       }
-    });
+  });
+
   })();
+
 
   $(document).mouseup(function (e) {
-    if (!menuWrap.is(e.target) && menuWrap.has(e.target).length === 0 && !cityCurrent.is(e.target) && cityCurrent.has(e.target).length === 0) {
-      menuWrap.parent().removeClass("_active");
-      citySearch.val("");
-      cityList.find("a").show();
+    if (!menuWrap.is(e.target)
+      && menuWrap.has(e.target).length === 0
+      && !cityCurrent.is(e.target)
+      && cityCurrent.has(e.target).length === 0) {
+        menuWrap.parent().removeClass("_active");
+        citySearch.val("");
+        cityList.find("a").show();
     }
   });
-}); //swiper js
+});
 
-var swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  slidesPerView: 1,
-  lazy: true,
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination'
-  },
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  }
+
+//swiper js
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+//   slidesPerView: 1,
+//   lazy: true,
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+// });
+
+
+//photoswipe js
+var $pswp = $('.pswp')[0];
+var image = [];
+
+//swiper js
+var mySwiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    on: {
+        init: function () {
+            var that = this;
+            var el = this.slides[this.activeIndex].querySelector('.section__servicesItemImg');
+            var observer = lozad(el, {
+                loaded: function (el) {
+                    var src = $(el).parent().parent().children('.section__servicesItemImg-link').children('.section__servicesItemImg').attr('src');
+                    var dataSrc = $(el).parent().parent().children('.section__servicesItemImg-link').children('.section__servicesItemImg').attr('data-srcset');
+                    $(el).parent().parent().children('.section__servicesItemImg-link').children('.section__servicesItemImg').attr("srcset", dataSrc);
+                    $(el).parent().parent().children('.section__servicesItemImg-bg').css('background-image', 'url(' + src + ')');
+                    $(el).parent().parent().children('.swiper-lazy-preloader').hide();
+                }
+            });
+            observer.observe();
+
+            $('.section__servicesItemImg-link').click(function (e) {
+                e.preventDefault();
+                if (!$(that.slides)[0].parentNode.classList.contains('active-gallery')) {
+                    $(that.slides)[0].parentNode.classList.add('active-gallery');
+                    var $pic = $($(that.slides)[0].parentNode.parentNode),
+                        getItems = function () {
+                            var items = [];
+                            $pic.find('p').each(function () {
+                                var $href = $(this).data('href'),
+                                    $size = $(this).data('size').split('x'),
+                                    $width = $size[0],
+                                    $height = $size[1];
+
+                                var item = {
+                                    src: $href,
+                                    w: $width,
+                                    h: $height
+                                }
+                                items.push(item);
+                            });
+                            return items;
+                        }
+
+                    var items = getItems();
+                    items.pop();
+                    items.shift();
+
+                    $.each(items, function (index, value) {
+                        image[index] = new Image();
+                        image[index].setAttribute('data-src', value['src']);
+                    });
+
+                    $pic.on('click', 'p', function (event) {
+                        event.preventDefault();
+                        var $index = that.realIndex;
+                        var options = {
+                            index: $index,
+                            bgOpacity: 0.7,
+                            showHideOpacity: true
+                        }
+
+                        var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
+                        lightBox.init();
+                        lightBox.preload = 1;
+                        lightBox.listen('beforeChange', function () {
+                            that.slideTo(this.getCurrentIndex() + 1, 300, true);
+                        });
+                    });
+                }
+            });
+        },
+        slideChange: function () {
+            var el = this.slides[this.activeIndex].querySelector('.section__servicesItemImg');
+            var observer = lozad(el, {
+                loaded: function (el) {
+                    var src = $(el).parent().parent().children('.section__servicesItemImg-link').children('.section__servicesItemImg').attr('src');
+                    var dataSrc = $(el).parent().parent().children('.section__servicesItemImg-link').children('.section__servicesItemImg').attr('data-srcset');
+                    $(el).parent().parent().children('.section__servicesItemImg-link').children('.section__servicesItemImg').attr("srcset", dataSrc);
+                    $(el).parent().parent().children('.section__servicesItemImg-bg').css('background-image', 'url(' + src + ')');
+                    $(el).parent().parent().children('.swiper-lazy-preloader').hide();
+                }
+            });
+            observer.observe();
+        }
+    },
 });
